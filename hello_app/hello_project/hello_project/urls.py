dbb41ad68b402a3afed35a175a852c51e27fa32a
Vasily Bortnikovdo <Vasily.Bortnikov@gmail.com>
2020-04-28 11:29:36 +0300

    ingress added

--- hello_app/hello_project/hello_project/urls.py
+++ hello_app/hello_project/hello_project/urls.py
@@ -18,7 +18,6 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    #path('hello/', include('hello_app.urls')),
     path('admin/', admin.site.urls),
     path('', include('hello_app.urls')),
 ]
