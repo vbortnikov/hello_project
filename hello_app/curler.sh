d6fa7a18b41c6074d86079490163dce279bbce98
Vasily Bortnikov <55582638+vbortnikov@users.noreply.github.com>
2020-05-12 05:18:51 +0300

    Update curler.sh

--- hello_app/curler.sh
+++ hello_app/curler.sh
@@ -1,4 +1,5 @@
-# helper to score up metrics
+# helper script (example) to score up metrics
+# I need it because Django uses CSRFToken
 curl  -c cookies.txt -b cookies.txt -s -w "%{http_code}" http://localhost:8000/
 token=$(grep srftoken cookies.txt | cut -f 7)
 echo  "X-CSRFToken:${token}"
