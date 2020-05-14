800fb7e6fe9f41ee24d381e8fe0d14235d7a8c3b
Vasily Bortnikovdo <Vasily.Bortnikov@gmail.com>
2020-05-14 07:00:36 +0300

    images added

--- hello_app/curler.sh
+++ hello_app/curler.sh
@@ -1,9 +1,9 @@
 # helper script (example) to score up metrics
 # I need it because Django uses CSRFToken
-curl  -c cookies.txt -b cookies.txt -s -w "%{http_code}" http://localhost:8000/
+curl  -c cookies.txt -b cookies.txt -s -w "%{http_code}" http://127.0.0.1:8000/
 token=$(grep srftoken cookies.txt | cut -f 7)
 echo  "X-CSRFToken:${token}"
-curl -c cookies.txt -b cookies.txt -s -H "X-CSRFToken:${token}" -w "%{http_code}"  -X POST http://localhost:8000/metrics/
-curl -c cookies.txt -b cookies.txt -s -H "X-CSRFToken:${token}" -w "%{http_code}"  -X POST http://localhost:8000/ready/
-curl -c cookies.txt -b cookies.txt -s -H "X-CSRFToken:${token}" -w "%{http_code}"  -X POST http://localhost:8000/health/
+curl -c cookies.txt -b cookies.txt -s -H "X-CSRFToken:${token}" -w "%{http_code}"  -X POST http://127.0.0.1:8000/metrics/
+curl -c cookies.txt -b cookies.txt -s -H "X-CSRFToken:${token}" -w "%{http_code}"  -X POST http://127.0.0.1:8000/ready/
+curl -c cookies.txt -b cookies.txt -s -H "X-CSRFToken:${token}" -w "%{http_code}"  -X POST http://127.0.0.1:8000/health/
 
